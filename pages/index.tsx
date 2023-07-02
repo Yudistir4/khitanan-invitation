e5f882075b24023691223b46a5210149c3b52f90
Yudistira Maulana <yudistir4maulana@gmail.com>
2023-07-03 00:18:01 +0700

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,155 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { Avatar, Button, Image, Text } from '@chakra-ui/react';
+import { Inter } from 'next/font/google';
+import { Dancing_Script } from 'next/font/google';
+import { useRouter } from 'next/router';
+import * as React from 'react';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
+const dancing = Dancing_Script({ subsets: ['latin'] });
 
 export default function Home() {
+  const [show, setShow] = React.useState(false);
+  const router = useRouter();
+  const query = router.query;
+  const untuk = query.untuk as string;
   return (
     <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+      className={`bg-[#305A5F] ${inter.className} w-full  overflow-hidden ${
+        show ? 'overflow-auto' : 'overflow-hidden max-h-screen'
+      }`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+      <div className={`max-w-md flex flex-col m-auto bg-[#305A5F] h-full `}>
+        {/* Opening */}
+        <div className="max-w-md  min-h-screen w-full bg-[url('/assets/bg2.png')] bg-cover bg-center relative flex flex-col  justify-center items-center">
+          <Image className="absolute top-0" src="/assets/atas.png" alt="atas" />
+          <Image
+            className="absolute bottom-0"
+            src="/assets/bawah.png"
+            alt="atas"
+          />
+
+          <div className="flex flex-col text-white text-center gap-4">
+            <h1 className="font-bold text-xl">Tasyakuran Khitan</h1>
+            <div
+              className={`flex flex-col font-bold text-2xl justify-center text-yellow-400 items-center ${dancing.className}`}
+            >
+              <h3>Sakha Virendra Shafwan</h3>
+              <p>&</p>
+              <h3>Gibran Aditya Ramadhan</h3>
+            </div>
+
+            <p className="font-bold">Sabtu, 08 Juli 2023</p>
+            {untuk && (
+              <>
+                <p>Untuk</p>
+                <p className="font-semibold">{untuk}</p>
+              </>
+            )}
+            <Button
+              as="a"
+              href="#profile"
+              onClick={() => setShow(true)}
+              borderRadius="full"
+            >
+              Buka Undangan
+            </Button>
+          </div>
+        </div>
+        {/* Profile */}
+        <div
+          id="profile"
+          className="bg-[#E4F6F8] py-10  gap-4 flex flex-col items-center justify-center p-4 text-center"
+        >
+          <h5 className={`font-bold text-3xl ${dancing.className}`}>
+            Assalamu&apos;alaikum Wr Wb
+          </h5>
+          <p className="text-lg text-black/80 mb-5">
+            Dengan Rahmat Allah yang Maha Kuasa Insha Allah kami akan
+            melangsungkan Syukuran Khitan
+          </p>
+
+          {/* TODO Countdown */}
+
+          <div className=" bg-white shadow-lg  shadow-black/30 rounded-xl p-4 flex flex-col gap-4 justify-center items-center">
+            <Avatar
+              className="shadow-xl"
+              src="/assets/orang.jpeg"
+              w="200px"
+              h="200px"
             />
-          </a>
+            <div
+              className={`flex flex-col font-bold text-3xl justify-center items-center ${dancing.className}`}
+            >
+              <h3>Sakha Virendra Shafwan</h3>
+              <p>&</p>
+              <h3>Gibran Aditya Ramadhan</h3>
+            </div>
+            <p className=" font-light">
+              Putra Ke-2 dan Ke-3 dari Bpk. Zainuddin & Ibu Yeni Widiastuti
+            </p>
+            <p className=" font-light">Di Khitan Pada: Sabtu, 24 Juni 2023</p>
+          </div>
+        </div>
+        {/* Acara */}
+        <div className="bg-[#E4F6F8]  py-10 gap-4 flex flex-col items-center justify-center p-4 text-center">
+          <h5 className={`font-bold text-3xl ${dancing.className}`}>
+            Undangan & Acara
+          </h5>
+          <p className="text-lg text-black/80 px-4 mb-5">
+            Bahagia rasanya apabila anda berkenan hadir dan memberikan doa
+          </p>
+
+          <div className=" bg-white shadow-lg shadow-black/30 rounded-xl p-4 flex flex-col gap-4 justify-center items-center">
+            <div>
+              <p className="font-semibold text-xl">Sabtu, 08 Juli 2023</p>
+              <p className="font-light text-xl">12:00 - 17:00 WIB</p>
+            </div>
+            <p className="font-light">
+              Komplek Kodam Jaya Jl. Seroja 2, Blok K2 No. 169, RT. 008 RW.005,
+              Kalideres Jakarta Barat
+            </p>
+          </div>
+        </div>
+        {/* Peta Lokasi */}
+        <div className="bg-[#E4F6F8] py-16  gap-6 flex flex-col items-center justify-center p-4 text-center">
+          <h5 className={`font-bold text-3xl ${dancing.className}`}>
+            Peta Lokasi
+          </h5>
+          <iframe
+            src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3966.8021301919002!2d106.69639431476878!3d-6.157249995542105!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zNsKwMDknMjYuMSJTIDEwNsKwNDEnNTQuOSJF!5e0!3m2!1sid!2sid!4v1688314676359!5m2!1sid!2sid"
+            // width="600"
+            // height="450"
+            className="w-full h-[300px] shadow-lg shadow-black/40"
+            // allowfullscreen={true}
+            loading="lazy"
+            // referrerpolicy="no-referrer-when-downgrade"
+          ></iframe>
+          <Button
+            as="a"
+            className="w-[80%]"
+            borderRadius="full"
+            href="https://goo.gl/maps/TYXsAhtYyJyDDuYu6"
+            colorScheme="blue"
+          >
+            Buka Lokasi
+          </Button>
+        </div>
+        {/* Kado Khitan */}
+        {/* Protokol Kesehatan */}
+        <div className="font-light bg-[#E4F6F8]  py-10 gap-4 flex flex-col items-center justify-center p-4 text-center">
+          <p>
+            Atas kehadiran dan doa restu Bapak/Ibu/Saudara/i, kami ucapkan
+            terimakasih{' '}
+          </p>
+          <p>Hormat Kami:</p>
+          <p className="font-semibold">
+            Kel. Bpk. Zainuddin & Ibu Yeni Widiastuti
+          </p>
+          <p className={`${dancing.className} text-2xl`}>
+            Wassalamualaikum Warrahmatullahi Wabarakatuh
+          </p>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
